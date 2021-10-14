3576b94e3cdd4167b7912d90fab2bfdef643a9f2
Christopher Phiri <chris7phiri@gmail.com>
2021-10-14 16:35:51 +0200

    added prisma

---
+++ koa/prisma/migrations/20211014141952_init/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE "Audio" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "path" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Playlist" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "path" TEXT NOT NULL,
+    "audioId" INTEGER NOT NULL,
+    CONSTRAINT "Playlist_audioId_fkey" FOREIGN KEY ("audioId") REFERENCES "Audio" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "AudioGrouped" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "audioId" INTEGER NOT NULL,
+    "playlistId" INTEGER NOT NULL,
+    CONSTRAINT "AudioGrouped_audioId_fkey" FOREIGN KEY ("audioId") REFERENCES "Audio" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "AudioGrouped_playlistId_fkey" FOREIGN KEY ("playlistId") REFERENCES "Playlist" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
